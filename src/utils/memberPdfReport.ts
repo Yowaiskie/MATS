4e297890b7aa0c0f08b95d27b1c551ddc280b457
Yowaiskie <kylemadriaga29@gmail.com>
2026-08-03 15:52:08 +0800

    brand: Update PWA and web branding to use newly installed transparent favicons

--- src/utils/memberPdfReport.ts
+++ src/utils/memberPdfReport.ts
@@ -49,7 +49,7 @@ export const downloadMembersReportPdf = async (
 
   // Try loading logo
   try {
-    const logoImg = await loadImage('/favicon.png')
+    const logoImg = await loadImage('/favicon/icon-192.png')
     doc.addImage(logoImg, 'PNG', 14, 10, 14, 14)
   } catch (err) {
     console.warn('Logo image could not be loaded for PDF:', err)
